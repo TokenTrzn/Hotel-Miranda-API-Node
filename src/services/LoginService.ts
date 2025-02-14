a18db00f39cb7ad31675c9dddbddba9296f2f596
Alberto <albertogilsanjuan@gmail.com>
2025-02-14 20:24:30 +0100

    ADD: Login Service

---
+++ src/services/LoginService.ts
@@ -0,0 +1,26 @@
+import { AuthInterface } from "../interfaces/AuthInterface";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import authData from '../data/authData.json'
+
+export class LoginService {
+
+    static async login(email: string, password: string): Promise<string | null> {
+        const user: AuthInterface[] = authData.filter(u => u.user === email)
+        if (user.length === 0) {
+            return null
+        }
+
+        const validPassword = await bcrypt.compare(password, user[0].password)
+        if (!validPassword) {
+            return null
+        }
+
+        const token = jwt.sign(
+            { email: user[0].user }, 
+            process.env.SECRET_TOKEN as string, 
+            { expiresIn: '1h' }
+        )
+        return token
+    }
+}
