3c20f72b676bbf1dd7f8ed2a1c87a586da71b030
Alberto <albertogilsanjuan@gmail.com>
2025-02-14 19:58:09 +0100

    FIX: Contact Swagger

--- src/controllers/ContactController.ts
+++ src/controllers/ContactController.ts
@@ -14,11 +14,11 @@ const baseUrl = '/contacts'
  * @swagger
  * /api/v1/contacts:
  *   get:
- *     summary: Obtiene una lista de contacts
+ *     summary: Get all contacts
  *     tags: [Contacts]
  *     responses:
  *       200:
- *         description: Lista de contacts
+ *         description: Get all contacts
  *         content:
  *           application/json:
  *             schema:
@@ -57,6 +57,48 @@ contactRouter.get(baseUrl, (req: Request, res: Response) => {
     res.json(contactList)
 })
 
+/**
+ * @swagger
+ * /api/v1/contacts/:id :
+ *   get:
+ *     summary: Get a contact by Id
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: Get contact by Id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                      type: number
+ *                      example: 1 
+ *                   date:
+ *                      type: string
+ *                      example: "08/30/2024"
+ *                   hour:
+ *                      type: string
+ *                      example: "5:18 PM"
+ *                   name:
+ *                      type: string
+ *                      example: "Alberto Gil"
+ *                   email:
+ *                      type: string
+ *                      example: "alberto@gmail.com"
+ *                   phone:
+ *                      type: string
+ *                      example: "593-825-5086"
+ *                   comment:
+ *                      type: string
+ *                      example: "Lorem ipsun"
+ *                   isArchived:
+ *                      type: boolean
+ *                      example: true              
+ */
+
 contactRouter.get(baseUrl + '/:id', (req: Request, res: Response) => {
     const contact = contactService.fetchById(parseInt(req.params.id))
     if (contact) {
@@ -66,10 +108,94 @@ contactRouter.get(baseUrl + '/:id', (req: Request, res: Response) => {
     }
 })
 
+/**
+ * @swagger
+ * /api/v1/contacts/create :
+ *   post:
+ *     summary: Create a contact
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: Create a contact
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                      type: number
+ *                      example: 1 
+ *                   date:
+ *                      type: string
+ *                      example: "08/30/2024"
+ *                   hour:
+ *                      type: string
+ *                      example: "5:18 PM"
+ *                   name:
+ *                      type: string
+ *                      example: "Alberto Gil"
+ *                   email:
+ *                      type: string
+ *                      example: "alberto@gmail.com"
+ *                   phone:
+ *                      type: string
+ *                      example: "593-825-5086"
+ *                   comment:
+ *                      type: string
+ *                      example: "Lorem ipsun"
+ *                   isArchived:
+ *                      type: boolean
+ *                      example: true              
+ */
+
 contactRouter.post(baseUrl, (req: Request, res: Response) => {
     const newContact = contactService.create(req.body)
     res.status(201).json(newContact)
 })
+
+/**
+ * @swagger
+ * /api/v1/contacts/:id :
+ *   put:
+ *     summary: Edit a contact
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: Edit a contact
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                      type: number
+ *                      example: 1 
+ *                   date:
+ *                      type: string
+ *                      example: "08/30/2024"
+ *                   hour:
+ *                      type: string
+ *                      example: "5:18 PM"
+ *                   name:
+ *                      type: string
+ *                      example: "Alberto Gil"
+ *                   email:
+ *                      type: string
+ *                      example: "alberto@gmail.com"
+ *                   phone:
+ *                      type: string
+ *                      example: "593-825-5086"
+ *                   comment:
+ *                      type: string
+ *                      example: "Lorem ipsun"
+ *                   isArchived:
+ *                      type: boolean
+ *                      example: true              
+ */
 
 contactRouter.put(baseUrl + '/:id', (req: Request, res: Response) => {
     const updateContact = contactService.update(parseInt(req.params.id), req.body)
@@ -79,6 +205,48 @@ contactRouter.put(baseUrl + '/:id', (req: Request, res: Response) => {
         res.status(404).json({ message: 'Contact not found' })
     }
 })
+
+/**
+ * @swagger
+ * /api/v1/contacts/:id :
+ *   delete:
+ *     summary: Delete a contact
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: Delete a contact
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                      type: number
+ *                      example: 1 
+ *                   date:
+ *                      type: string
+ *                      example: "08/30/2024"
+ *                   hour:
+ *                      type: string
+ *                      example: "5:18 PM"
+ *                   name:
+ *                      type: string
+ *                      example: "Alberto Gil"
+ *                   email:
+ *                      type: string
+ *                      example: "alberto@gmail.com"
+ *                   phone:
+ *                      type: string
+ *                      example: "593-825-5086"
+ *                   comment:
+ *                      type: string
+ *                      example: "Lorem ipsun"
+ *                   isArchived:
+ *                      type: boolean
+ *                      example: true              
+ */
 
 contactRouter.delete(baseUrl + '/:id', (req: Request, res: Response) => {
     const deletedContact = contactService.delete(parseInt(req.params.id))
