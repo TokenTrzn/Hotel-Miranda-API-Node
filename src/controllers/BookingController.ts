d25ac481caac51c03dfdc8e109669dc4c16bc487
Alberto <albertogilsanjuan@gmail.com>
2025-02-14 19:54:28 +0100

    FIX: Bookings Swagger

--- src/controllers/BookingController.ts
+++ src/controllers/BookingController.ts
@@ -3,7 +3,7 @@ import { BookingService } from "../services/BookingService"
 
 export const bookingRouter = Router()
 const bookingService = new BookingService()
-const baseUrl = '/bookings'
+const baseUrl = 'api/v1/bookings'
 
 /**
  * @swagger
@@ -15,11 +15,11 @@ const baseUrl = '/bookings'
  *  @swagger
  * /api/v1/bookings :
  *   get:
- *     summary: Obtiene una lista de bookings
+ *     summary: Get all bookings
  *     tags: [Bookings]
  *     responses:
  *       200:
- *         description: Lista de bookings
+ *         description: Get all bookings
  *         content:
  *           application/json:
  *             schema:
@@ -79,6 +79,69 @@ bookingRouter.get(baseUrl, (req: Request, res: Response) => {
     res.json(bookingList)
 })
 
+/**
+ *  @swagger
+ * /api/v1/bookings/:id :
+ *   get:
+ *     summary: Get a booking by Id
+ *     tags: [Bookings]
+ *     responses:
+ *       200:
+ *         description: Get a booking by Id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   guestName:
+ *                     type: string
+ *                     example: "https://urlFoto.com"
+ *                   orderDate:
+ *                     type: string
+ *                     example: "07/30/2042"
+ *                   orderDateHour:
+ *                     type: string
+ *                     example: "6:53 PM"
+ *                   checkIn:
+ *                     type: string
+ *                     example: "10/16/2029"
+ *                   checkInHour:
+ *                      type: string
+ *                      example: "7:45 AM"
+ *                   checkOut: 
+ *                      type: string
+ *                      example: "10/16/2029"
+ *                   checkOutHour:
+ *                      type: string
+ *                      example: "7:45 PM"
+ *                   specialRequest: 
+ *                      type: string
+ *                      example: "true"
+ *                   type:
+ *                      type: string
+ *                      example: "Premium A"
+ *                   number:
+ *                      type: number
+ *                      example: 5643
+ *                   status:
+ *                      type: string
+ *                      example: "Check In"
+ *                   price:
+ *                      type: string
+ *                      example: "$101.97"
+ *                   description:
+ *                      type: string
+ *                      example: "Lorem ipsum"
+ *                   amenities: 
+ *                      type: string[]
+ *                      example: ['Wifi', 'Mini Bar']
+ */
+
 bookingRouter.get(baseUrl + '/:id', (req: Request, res: Response) => {
     const booking = bookingService.fetchById(parseInt(req.params.id))
     if (booking) {
@@ -88,10 +151,136 @@ bookingRouter.get(baseUrl + '/:id', (req: Request, res: Response) => {
     }
 })
 
+/**
+ *  @swagger
+ * /api/v1/bookings/create :
+ *   post:
+ *     summary: Create a booking
+ *     tags: [Bookings]
+ *     responses:
+ *       200:
+ *         description: Create a booking
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   guestName:
+ *                     type: string
+ *                     example: "https://urlFoto.com"
+ *                   orderDate:
+ *                     type: string
+ *                     example: "07/30/2042"
+ *                   orderDateHour:
+ *                     type: string
+ *                     example: "6:53 PM"
+ *                   checkIn:
+ *                     type: string
+ *                     example: "10/16/2029"
+ *                   checkInHour:
+ *                      type: string
+ *                      example: "7:45 AM"
+ *                   checkOut: 
+ *                      type: string
+ *                      example: "10/16/2029"
+ *                   checkOutHour:
+ *                      type: string
+ *                      example: "7:45 PM"
+ *                   specialRequest: 
+ *                      type: string
+ *                      example: "true"
+ *                   type:
+ *                      type: string
+ *                      example: "Premium A"
+ *                   number:
+ *                      type: number
+ *                      example: 5643
+ *                   status:
+ *                      type: string
+ *                      example: "Check In"
+ *                   price:
+ *                      type: string
+ *                      example: "$101.97"
+ *                   description:
+ *                      type: string
+ *                      example: "Lorem ipsum"
+ *                   amenities: 
+ *                      type: string[]
+ *                      example: ['Wifi', 'Mini Bar']
+ */
+
 bookingRouter.post(baseUrl, (req: Request, res: Response) => {
     const newBooking = bookingService.create(req.body)
     res.status(201).json(newBooking)
 })
+
+/**
+ *  @swagger
+ * /api/v1/bookings/:id :
+ *   put:
+ *     summary: Edit a booking
+ *     tags: [Bookings]
+ *     responses:
+ *       200:
+ *         description: Edit a booking
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   guestName:
+ *                     type: string
+ *                     example: "https://urlFoto.com"
+ *                   orderDate:
+ *                     type: string
+ *                     example: "07/30/2042"
+ *                   orderDateHour:
+ *                     type: string
+ *                     example: "6:53 PM"
+ *                   checkIn:
+ *                     type: string
+ *                     example: "10/16/2029"
+ *                   checkInHour:
+ *                      type: string
+ *                      example: "7:45 AM"
+ *                   checkOut: 
+ *                      type: string
+ *                      example: "10/16/2029"
+ *                   checkOutHour:
+ *                      type: string
+ *                      example: "7:45 PM"
+ *                   specialRequest: 
+ *                      type: string
+ *                      example: "true"
+ *                   type:
+ *                      type: string
+ *                      example: "Premium A"
+ *                   number:
+ *                      type: number
+ *                      example: 5643
+ *                   status:
+ *                      type: string
+ *                      example: "Check In"
+ *                   price:
+ *                      type: string
+ *                      example: "$101.97"
+ *                   description:
+ *                      type: string
+ *                      example: "Lorem ipsum"
+ *                   amenities: 
+ *                      type: string[]
+ *                      example: ['Wifi', 'Mini Bar']
+ */
 
 bookingRouter.put(baseUrl + '/:id', (req: Request, res: Response) => {
     const updatedBooking = bookingService.update(parseInt(req.params.id), req.body)
@@ -101,6 +290,69 @@ bookingRouter.put(baseUrl + '/:id', (req: Request, res: Response) => {
         res.status(404).json({ message: 'Booking not found' })
     }
 })
+
+/**
+ *  @swagger
+ * /api/v1/bookings/:id :
+ *   delete:
+ *     summary: Delete a booking
+ *     tags: [Bookings]
+ *     responses:
+ *       200:
+ *         description: Delete a booking
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: number
+ *                     example: 1
+ *                   guestName:
+ *                     type: string
+ *                     example: "https://urlFoto.com"
+ *                   orderDate:
+ *                     type: string
+ *                     example: "07/30/2042"
+ *                   orderDateHour:
+ *                     type: string
+ *                     example: "6:53 PM"
+ *                   checkIn:
+ *                     type: string
+ *                     example: "10/16/2029"
+ *                   checkInHour:
+ *                      type: string
+ *                      example: "7:45 AM"
+ *                   checkOut: 
+ *                      type: string
+ *                      example: "10/16/2029"
+ *                   checkOutHour:
+ *                      type: string
+ *                      example: "7:45 PM"
+ *                   specialRequest: 
+ *                      type: string
+ *                      example: "true"
+ *                   type:
+ *                      type: string
+ *                      example: "Premium A"
+ *                   number:
+ *                      type: number
+ *                      example: 5643
+ *                   status:
+ *                      type: string
+ *                      example: "Check In"
+ *                   price:
+ *                      type: string
+ *                      example: "$101.97"
+ *                   description:
+ *                      type: string
+ *                      example: "Lorem ipsum"
+ *                   amenities: 
+ *                      type: string[]
+ *                      example: ['Wifi', 'Mini Bar']
+ */
 
 bookingRouter.delete(baseUrl + '/:id', (req: Request, res: Response) => {
     const deletedBooking = bookingService.delete(parseInt(req.params.id))
