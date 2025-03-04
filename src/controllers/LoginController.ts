7d64ed79d38be0916584d19b8d80d5ee36395f15
Alberto <albertogilsanjuan@gmail.com>
2025-03-04 13:24:45 +0100

    FIX: Login Controller

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from "express"
+import { NextFunction, Request, Response, Router } from "express"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { UserInterface } from '../interfaces/UserInterface'
@@ -6,30 +6,29 @@ import { UserService } from "../services/UserService"
 
 export const loginRouter = Router()
 
-loginRouter.post('/login', async (req: Request, res: Response) => {
+loginRouter.post('/login', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { email, password } = req.body
-
     const userService = new UserService()
-    const userData = await userService.fetchAll()
-    const user: UserInterface | undefined = userData.find(u => u.email === email) 
-    if (user === undefined) {
-        res.status(400).send('User not found')
-        return
-    }
-    if (!process.env.SECRET_TOKEN) {
-        res.status(500).send('No token provided')
-        return
-    }
 
-    bcrypt.compare(password, user.password)
-        .then((result) => {
-            if (!result) {
-                res.status(400).send({ token: 'Usuario o contraseña incorrectos' })
-                return
-            } else {
-                const token = jwt.sign({ email: user.email }, process.env.SECRET_TOKEN as string, { expiresIn: '1h' })
-                res.status(200).send({ token: token })
-            }
-        })
-
+    try {
+        const userData = await userService.fetchAll()
+        const user: UserInterface | undefined = userData.find(u => u.email === email)
+        if (user === undefined) {
+            res.status(400).send('User not found')
+            return
+        }        
+        if (password !== user.password) {
+            res.status(401).json({ message: 'Incorrect Password' })
+            return
+        }
+        if (!process.env.SECRET_TOKEN) {
+            res.status(500).send('No token provided')
+            return
+        }
+        const token = jwt.sign({ email: user.email }, process.env.SECRET_TOKEN as string, { expiresIn: '1h' })
+        res.status(200).json({ token, email: user.email, name: user.name })
+        
+    } catch (error) {
+        throw error
+    }
 })
